0f635fe4644da83868d262872f709c977f3fc346
jiunwei <jiunwei@gmail.com>
2018-10-18 09:14:25 +0800

    Add jquery and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
 
+require('jquery/dist/jquery.slim');
+require('bootstrap/dist/css/bootstrap.css');
+require('bootstrap/dist/js/bootstrap.bundle.min');
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    const pluginsTriggerElement = document.getElementById('plugins-trigger');
-    const pluginsElement = document.getElementById('plugins');
+    // const pluginsTriggerElement = document.getElementById('plugins-trigger');
+    // const pluginsElement = document.getElementById('plugins');
 
-    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+    // const pluginsVisibleClass = "splash-overview-plugins__list--visible";
 
-    pluginsTriggerElement.onclick = () => {
-        pluginsElement.classList.toggle(pluginsVisibleClass);
-    }
+    // pluginsTriggerElement.onclick = () => {
+    //     pluginsElement.classList.toggle(pluginsVisibleClass);
+    // }
+
 });
